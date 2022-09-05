2dd5c831e80bdea9b930a9b5866098cf70dc8ce4
rmbodegalibre <rmbodegalibre@gmail.com>
2022-09-04 20:10:37 -0500

    main.c

---
+++ main.c
@@ -0,0 +1,23 @@
+/**
+ * main - monty
+ * @argc: integer number of arguments
+ * @argv: Array that contains arguments
+ * Return: Always 0
+ */
+
+int main(int argc, char **argv)
+{
+	if (argc < 2 || argc > 2)
+	{
+		dprintf(2, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+/*Check for the file - if i get the path from argv*/
+/*		printf("First %s \n", argv[1]);*/
+		open_file(argv[1]);
+	}
+	m_free();
+	return (0);
+}
